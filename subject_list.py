2144570d5f390adc127a8c7f9acee694ad0b0a78
Leidy Alexandra Garavito Paez <lgp@sempli.co>
2022-10-24 23:25:03 -0500

    subiendo imagenes

--- subject_list.py
+++ subject_list.py
@@ -5,7 +5,7 @@ number_of_subject = int(input('ingrese la cantidad de materias: '))
 for i in range(number_of_subject):
   subject = input('Ingrese el nombre de la materia '+ str(i+1) + ':')
   subject_list.append(subject)
-  student_note = input('Que nota desea para esta materia de 0 a 5')
+  student_note = input('Que nota desea para esta materia de 0 a 5:')
   subject_list.append(student_note)
 
 print(subject_list)
